06b5a1a75dd751940915027bdfa8e4ad602c905b
alhaura19 <haura19@gmail.com>
2018-01-22 20:45:54 +0700

    first

---
+++ PhotoShot/photoshot.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace PhotoShot
+{
+    partial class Photoshot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Photoshot));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoPlayer = new System.Windows.Forms.PictureBox();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.displayMiniImg = new System.Windows.Forms.PictureBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.filterName = new MetroFramework.Controls.MetroLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).BeginInit();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.displayMiniImg)).BeginInit();
+            this.metroTabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(148, 183);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // videoPlayer
+            // 
+            this.videoPlayer.Location = new System.Drawing.Point(0, 3);
+            this.videoPlayer.Name = "videoPlayer";
+            this.videoPlayer.Size = new System.Drawing.Size(340, 343);
+            this.videoPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.videoPlayer.TabIndex = 0;
+            this.videoPlayer.TabStop = false;
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.ItemSize = new System.Drawing.Size(100, 40);
+            this.metroTabControl1.Location = new System.Drawing.Point(7, 63);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(348, 464);
+            this.metroTabControl1.TabIndex = 6;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.displayMiniImg);
+            this.metroTabPage1.Controls.Add(this.metroButton1);
+            this.metroTabPage1.Controls.Add(this.videoPlayer);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 44);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(340, 416);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Home";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // displayMiniImg
+            // 
+            this.displayMiniImg.Location = new System.Drawing.Point(12, 352);
+            this.displayMiniImg.Name = "displayMiniImg";
+            this.displayMiniImg.Size = new System.Drawing.Size(71, 59);
+            this.displayMiniImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.displayMiniImg.TabIndex = 9;
+            this.displayMiniImg.TabStop = false;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("metroButton1.BackgroundImage")));
+            this.metroButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.metroButton1.Location = new System.Drawing.Point(128, 352);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(61, 59);
+            this.metroButton1.TabIndex = 7;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.metroLabel6);
+            this.metroTabPage2.Controls.Add(this.metroLabel5);
+            this.metroTabPage2.Controls.Add(this.metroLabel4);
+            this.metroTabPage2.Controls.Add(this.metroLabel3);
+            this.metroTabPage2.Controls.Add(this.metroLabel2);
+            this.metroTabPage2.Controls.Add(this.metroLabel1);
+            this.metroTabPage2.Controls.Add(this.pictureBox2);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 44);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(340, 416);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "About";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(88, 218);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(159, 19);
+            this.metroLabel6.TabIndex = 11;
+            this.metroLabel6.Text = "Teknik Informatika © 2016";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(7, 92);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(135, 19);
+            this.metroLabel5.TabIndex = 10;
+            this.metroLabel5.Text = "Muhammad Niudanri";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(7, 73);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(88, 19);
+            this.metroLabel4.TabIndex = 9;
+            this.metroLabel4.Text = "Afta Nugraha";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(7, 54);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(81, 19);
+            this.metroLabel3.TabIndex = 8;
+            this.metroLabel3.Text = "Fitria Fiviyan";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(7, 35);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(96, 19);
+            this.metroLabel2.TabIndex = 7;
+            this.metroLabel2.Text = "Latifah Alhaura";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(7, 16);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(93, 19);
+            this.metroLabel1.TabIndex = 6;
+            this.metroLabel1.Text = "Developed by:";
+            // 
+            // filterName
+            // 
+            this.filterName.AutoSize = true;
+            this.filterName.Location = new System.Drawing.Point(128, 526);
+            this.filterName.Name = "filterName";
+            this.filterName.Size = new System.Drawing.Size(0, 0);
+            this.filterName.TabIndex = 7;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            // 
+            // Photoshot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 550);
+            this.Controls.Add(this.filterName);
+            this.Controls.Add(this.metroTabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Photoshot";
+            this.Text = "Photoshot";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Photoshot_FormClosing);
+            this.Load += new System.EventHandler(this.Photoshot_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).EndInit();
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.displayMiniImg)).EndInit();
+            this.metroTabPage2.ResumeLayout(false);
+            this.metroTabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox videoPlayer;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.PictureBox displayMiniImg;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MetroFramework.Controls.MetroLabel filterName;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ PhotoShot/photoshot.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Forms;
+
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using MetroFramework.Forms;
+
+namespace PhotoShot
+{
+    public partial class Photoshot : MetroForm
+    {
+        static Bitmap camera = null;
+        
+        public ObservableCollection<FilterInfo> CameraDevice; // kamera yang dapat digunakan
+        
+        public VideoCaptureDevice VideoFrame; // untuk hasil capture
+
+        public Photoshot()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Photoshot_Load(object sender, EventArgs e)
+        {
+            //CameraDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice); // constructor
+            CameraDevice = new ObservableCollection<FilterInfo>();
+            foreach (FilterInfo filterInfo in new FilterInfoCollection(FilterCategory.VideoInputDevice))
+            {
+                CameraDevice.Add(filterInfo);
+            }
+
+            VideoFrame = new VideoCaptureDevice(CameraDevice[0].MonikerString);
+            VideoFrame.NewFrame += new NewFrameEventHandler(video_NewFrame);
+
+            ThreadStart f = new ThreadStart(VideoFrame.Start);
+            Thread thread = new Thread(f);
+            thread.Start();
+            
+        }
+
+        private void video_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            //jalankan frame
+            camera = (Bitmap)eventArgs.Frame.Clone();
+
+            videoPlayer.Image = camera;
+            Thread.Sleep(10);
+
+            Sepia sp = new Sepia();
+            videoPlayer.Image = sp.Apply(camera);
+            Thread.Sleep(10);
+
+            Invert i = new Invert();
+            videoPlayer.Image = i.Apply(camera);
+            Thread.Sleep(10);
+
+            Grayscale g = new Grayscale(0.2125, 0.7154, 0.0721);
+            videoPlayer.Image = g.Apply(camera);
+            Thread.Sleep(10);
+
+            
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            displayMiniImg.Image = (Bitmap)videoPlayer.Image.Clone();
+            
+            Bitmap current = (Bitmap)videoPlayer.Image.Clone();
+            string filepath = @"D:\Files\SEMESTER 7\Real Time Pro\archive\PhotoShot\Output\image1.jpg";
+            //string filepath = Environment.CurrentDirectory;
+            int i = 0;
+            while (File.Exists(filepath))
+            {
+                i++;
+                filepath = @"D:\Files\SEMESTER 7\Real Time Pro\archive\PhotoShot\Output\image" + i +".jpg";
+            }
+            string fileName = System.IO.Path.Combine(filepath);
+            current.Save(fileName);
+            current.Dispose();
+        }
+
+        private void Photoshot_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            VideoFrame.Stop();
+        }
+        
+
+    }
+
+}
